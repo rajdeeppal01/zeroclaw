60e9991dd055c2218a98ce94e88a20de504dee19
rajdeep <rajdeep.pal2004@gmail.com>
2026-08-14 02:36:54 +0530

    chore(pki): atomic writes for CRL rebuilt, normalize CRLF to LF for existing scripts

--- pki/renew_crl.sh
+++ pki/renew_crl.sh
@@ -8,7 +8,9 @@ echo "[$(date)] === Regenerating Intermediate CRL ==="
 openssl ca -gencrl -config ca_db_inter/ca.conf -out crl/intermediate.crl
 
 echo "[$(date)] === Rebuilding Combined CRL ==="
-cat crl/root.crl crl/intermediate.crl > crl/ca-chain.crl
+cat crl/root.crl crl/intermediate.crl > crl/ca-chain.crl.tmp
+openssl crl -in crl/ca-chain.crl.tmp -noout
+mv crl/ca-chain.crl.tmp crl/ca-chain.crl
 
 echo "[$(date)] CRL successfully renewed."
 
